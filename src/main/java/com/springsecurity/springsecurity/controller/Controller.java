9c6461ea3c2e3bd1eb090e66bc63b835485c5051
AsadullahShaikh123 <asadullahshaikh713@gmail.com>
2024-06-04 15:39:41 +0500

    added pre authorized annotation

--- src/main/java/com/springsecurity/springsecurity/controller/Controller.java
+++ src/main/java/com/springsecurity/springsecurity/controller/Controller.java
@@ -1,5 +1,6 @@
 package com.springsecurity.springsecurity.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,18 @@ public class Controller {
 	@GetMapping("/hello")
 	public String greetings() {
 		return "Hello from me ";
+	}
+	
+	@PreAuthorize("hasRole('USER')")
+	@GetMapping("/user")
+	public String userEndpoint() {
+		return "Hello User ";
+	}
+	
+	
+	@PreAuthorize("hasRole('ADMIN')")
+	@GetMapping("/admin")
+	public String adminEndPoint() {
+		return "Hello, Admin ";
 	}
 }
